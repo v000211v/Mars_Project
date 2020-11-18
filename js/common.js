86184d736c5acd9058daa3d23d88301fbe020217
v000211v <v000211v@naver.com>
2020-11-18 12:19:11 +0900

    r

--- js/common.js
+++ js/common.js
@@ -1,4 +1,15 @@
 $(function () {
+  // 모바일 전체메뉴
+  var menu = $('.menu_list > li > a')
+  menu.click(function () {
+    var subMenu = $(this).next('ul')
+    if (subMenu.is(':visible')) {
+      subMenu.slideUp()
+    } else {
+      subMenu.slideDown()
+    }
+  })
+
   // 전체메뉴
   var hamburger = $('.hamburger')
   var close = $('.menu .close')
